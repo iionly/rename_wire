dd90650ca53d2231621ce64726503426881b2656
iionly <iionly@gmx.de>
2014-09-28 20:45:23 +0200

    Initial commit

---
+++ rename_wire/languages/en.php
@@ -0,0 +1,63 @@
+<?php
+/* Modify the langauage string according to your needs to customize The Wire branding
+ *
+ * Save the changed file in UTF8 without BOM encoding
+ */
+
+return array(
+
+	/**
+	 * Menu items and titles
+	 */
+	'thewire' => "Posts",
+	'thewire:everyone' => "All Posts",
+	'thewire:user' => "%s's Posts",
+	'thewire:friends' => "Friends' Posts",
+	'thewire:replying' => "Replying to %s (@%s) who wrote",
+	'thewire:thread' => "Thread",
+	'thewire:charleft' => "characters remaining",
+	'thewire:tags' => "Posts tagged with '%s'",
+	'thewire:noposts' => "No Posts yet",
+	'item:object:thewire' => "Posts",
+	'thewire:by' => 'Post by %s',
+	'thewire:previous:help' => "View previous Post",
+	'thewire:hide:help' => "Hide previous Post",
+
+	/**
+	 * The wire river
+	 */
+	'river:create:object:thewire' => "%s posted to %s",
+	'thewire:wire' => 'Post',
+
+	/**
+	 * Wire widget
+	 */
+	'thewire:widget:desc' => 'Display your latest Posts',
+	'thewire:num' => 'Number of Posts to display',
+	'thewire:moreposts' => 'More Posts',
+
+	/**
+	 * Status messages
+	 */
+	'thewire:posted' => "Your message was successfully posted.",
+	'thewire:deleted' => "The Post was successfully deleted.",
+	'thewire:blank' => "Sorry, you need to enter some text before we can post this.",
+	'thewire:notfound' => "Sorry, we could not find the specified Post.",
+	'thewire:notsaved' => "Sorry. We could not save this Post.",
+	'thewire:notdeleted' => "Sorry. We could not delete this Post.",
+
+	/**
+	 * Notifications
+	 */
+	'thewire:notify:summary' => 'New Post: %s',
+	'thewire:notify:subject' => "New Post from %s",
+	'thewire:notify:reply' => '%s responded to %s in a Post:',
+	'thewire:notify:post' => '%s posted:',
+	'thewire:notify:footer' => "View and reply:\n%s",
+
+	/**
+	 * Settings
+	 */
+	'thewire:settings:limit' => "Maximum number of characters for Posts:",
+	'thewire:settings:limit:none' => "No limit",
+);
